579d48b5bd6628203d46faba7d28aeaa1c033f34
HaHuy2004 <haduchuy122004@gmail.com>
2026-01-11 17:15:15 +0700

    update eval enrich

--- enriched_eval/__init__.py
+++ enriched_eval/__init__.py
@@ -6,11 +6,14 @@ Based on ICLR 2025 Paper: Bridging Information Asymmetry
 from .query_generator import generate_enriched_queries
 from .fqs_selector import farthest_query_selection
 from .voting_aggregator import majority_voting_aggregation
-from .enriched_dataloader import create_enriched_dataloader
+
+# NOTE: enriched_dataloader is currently unused in main evaluation code
+# Commented out to avoid import issues - uncomment if needed in future
+# from .enriched_dataloader import create_enriched_dataloader
 
 __all__ = [
     'generate_enriched_queries',
     'farthest_query_selection',
     'majority_voting_aggregation',
-    'create_enriched_dataloader'
+    # 'create_enriched_dataloader'  # Commented - currently unused
 ]
